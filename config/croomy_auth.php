2932d9145747de16c004b5f176a04327069b7dfe
David M. Lemcoe Jr <david@lemcoe.com>
2012-11-18 03:20:53 +0000

    Updated the config file to get rid of the old captcha files.

--- config/croomy_auth.php
+++ config/croomy_auth.php
@@ -134,28 +134,6 @@ $config['forgot_password_expire'] = 60*15;
 
 /*
 |--------------------------------------------------------------------------
-| Captcha
-|
-| You can set captcha that created by Auth library in here.
-| 'captcha_path' = Directory where the catpcha will be created.
-| 'captcha_fonts_path' = Font in this directory will be used when creating captcha.
-| 'captcha_font_size' = Font size when writing text to captcha. Leave blank for random font size.
-| 'captcha_grid' = Show grid in created captcha.
-| 'captcha_expire' = Life time of created captcha before expired, default is 3 minutes (180 seconds).
-| 'captcha_case_sensitive' = Captcha case sensitive or not.
-|--------------------------------------------------------------------------
-*/
-$config['captcha_path'] = 'captcha/';
-$config['captcha_fonts_path'] = 'captcha/fonts/5.ttf';
-$config['captcha_width'] = 200;
-$config['captcha_height'] = 50;
-$config['captcha_font_size'] = 14;
-$config['captcha_grid'] = FALSE;
-$config['captcha_expire'] = 180;
-$config['captcha_case_sensitive'] = TRUE;
-
-/*
-|--------------------------------------------------------------------------
 | reCAPTCHA
 |
 | 'use_recaptcha' = Use reCAPTCHA instead of common captcha
